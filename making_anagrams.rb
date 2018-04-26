283f2e2a819a1ee2af1e6b149a3f066c3a6cb74d
chao <mailbox.chao.cao@gmail.com>
2018-04-26 16:10:29 -0700

    String anagrams

---
+++ making_anagrams.rb
@@ -0,0 +1,8 @@
+Make a hash counter hash of letters in (a) and (b)
+a {
+ a: [1][2],
+ b: [2][2],
+ c: [2][1]
+}
+For each letter find the difference between the first and the second value
+Add together the sum of the differences for each letter 
