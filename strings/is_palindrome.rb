3ce30a38b2b6d0d430ca79d21712bf167b2a502a
chao <mailbox.chao.cao@gmail.com>
2018-05-26 23:09:22 -0700

    is_palindrome

---
+++ strings/is_palindrome.rb
@@ -0,0 +1,5 @@
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Example 1:
+#
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
