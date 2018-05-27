9a40807551bc6a1bf0f0be2d5c815ae5365011c8
chao <mailbox.chao.cao@gmail.com>
2018-05-26 23:09:53 -0700

    is_palindrome solution

--- strings/is_palindrome.rb
+++ strings/is_palindrome.rb
@@ -3,3 +3,12 @@
 #
 # Input: "A man, a plan, a canal: Panama"
 # Output: true
+
+def is_palindrome(s)
+    alpha_num = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    lets = s.downcase.chars.select {|ch| alpha_num.include?(ch)}
+    lets.each_with_index do |ch, i|
+        return false if lets[i] != lets[lets.length - i - 1]
+    end
+    true
+end
