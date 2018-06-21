f219cdba106dc5f249b4b671f90d399ef86ceab9
chao <mailbox.chao.cao@gmail.com>
2018-06-20 18:00:55 -0700

    Add minimum depth solution explanation

--- trees/binary_trees/min_depth.rb
+++ trees/binary_trees/min_depth.rb
@@ -14,3 +14,21 @@
 #     /  \
 #    15   7
 # return its minimum depth = 2.
+
+def min_depth(root)
+  # 1) depth is 0 if we have no root
+  return 0 if root.nil?
+
+  # 2) find depths of subtrees deeper down the tree
+  right = min_depth(root.right)
+  left = min_depth(root.left)
+
+  # 3) if either child is nil we go down the other path
+  # we add together both roots so we don't need a messy conditional
+  if left == 0 || right == 0
+    left + right + 1
+  else
+    # 4) if both roots have 2 children we go with the shorter depth
+    [left, right].min + 1
+  end
+end
